8a100a5362a51ece8988d3589a265f7a769a84c1
Your Name <you@example.com>
2015-09-14 12:25:27 -0400

    fix

--- dbtest.php
+++ dbtest.php
@@ -6,13 +6,13 @@ $dbpwd = getenv("MYSQL_PASSWORD");
 $dbname = getenv("MYSQL_DATABASE");
 
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-if ($mysqli->connect_errno) {
+if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
 echo "<br><br><br><br><h1>My awesome PHP test</h1>";
 $query = "SELECT * from users";
-$rs = mysqli->query($query);
+$rs = connection->query($query);
 while ($row = $rs->fetch_array(MYSQLI_ASSOC)) {
     echo $row['user_id'] . " " . $row['username'] . "\n";
 }
