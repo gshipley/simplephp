846640f85f7c5e4184ce496cba1a988f065fb88a
Your Name <you@example.com>
2015-09-14 12:21:16 -0400

    Test

--- dbtest.php
+++ dbtest.php
@@ -4,19 +4,18 @@ $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("MYSQL_USER");
 $dbpwd = getenv("MYSQL_PASSWORD");
 $dbname = getenv("MYSQL_DATABASE");
-$connection = mysql_connect($dbhost, $dbuser, $dbpwd);
-if (!$connection) {
-        echo "Could not connect to database!!!";
-} else {
-        echo "Connected to database.<br>";
-}
 
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
 echo "<br><br><br><br><h1>My awesome PHP test</h1>";
-$dbconnection = mysql_select_db($dbname);
 $query = "SELECT * from users";
-$rs = mysql_query($query);
-while ($row = mysql_fetch_assoc($rs)) {
+$rs = mysqli->query($query);
+while ($row = $rs->fetch_array(MYSQLI_ASSOC)) {
     echo $row['user_id'] . " " . $row['username'] . "\n";
 }
-mysql_close();
+$rs->close();
+$connection->close();
 ?>
