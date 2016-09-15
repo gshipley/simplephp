7c574195d264ddd980aeb16dc9583cd83b5b4a44
Grant Shipley <gshipley@gmail.com>
2016-09-15 19:14:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 echo "Welcome to the best application in the world.";
 
-echo "<br>Welcome to the QBR.  Sales lots of OpenShift.";
+echo "<br>Welcome to the Raleigh, the best place on earth.";
 
 
