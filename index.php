e1869035d6e8169f39bc274e09dd11925560dbbb
Grant Shipley <gshipley@gmail.com>
2016-10-10 12:33:58 -0400

    how I want it

--- index.php
+++ index.php
@@ -3,3 +3,10 @@
 echo "Welcome to the best application in the world.";
 
 echo "<br><br>Github sync";
+
+echo "<br><br>rsync in action";
+echo "<br>hi";
+echo "<br>hi";
+echo "<br>hi";
+echo "<br>hi";
+
