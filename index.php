d32b29938c7d6897d4937a76f206df976544f42a
Grant Shipley <gshipley@gmail.com>
2016-10-10 10:31:19 -0400

    Update index.php
    update

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
 
 echo "Welcome to the best application in the world.";
-
-
-echo "<br>Rsync is awesome";
-echo "<br>Rsync is awesome";
-echo "<br>Rsync is awesome";
-echo "<br>Rsync is awesome";
-echo "<br>Rsync is awesome";
-echo "<br>Rsync is awesome";
-
-
