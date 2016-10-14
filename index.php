6fb5b444e81967a87c8c8f988cd3a49cdd20929b
grant shipley <grant shipley>
2016-10-14 15:51:50 -0400

    some changes

--- index.php
+++ index.php
@@ -4,3 +4,8 @@ echo "Welcome to the best application in the world.";
 
 echo "<br>All running on openshift";
 
+echo "<br> rsync is awesome";
+echo "<br> rsync is awesome";
+echo "<br> rsync is awesome";
+echo "<br> rsync is awesome";
+
