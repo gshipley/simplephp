710b9e8fac15e00bd03864d512749e4888a93ca8
Your Name <you@example.com>
2016-11-17 18:59:07 -0500

    added stuff

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
 echo "<h1>Welcome to the best application in the world.";
+echo "<br><h1>Welcome to the best application in the world.";
+echo "<br>Stop using h1 tags, it hurts my eyes";
 
 
