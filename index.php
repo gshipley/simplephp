0b9f5e90c4d3224b0acc2aebc2df742951edac48
Your Name <you@example.com>
2016-11-17 19:04:07 -0500

    my changes

--- index.php
+++ index.php
@@ -4,4 +4,11 @@ echo "<h1>Welcome to the best application in the world.";
 echo "<br><h1>Welcome to the best application in the world.";
 echo "<br>Stop using h1 tags, it hurts my eyes";
 
+echo "</h1> forgot to close tag";
+echo "</h1> forgot to close tag";
+echo "</h1> forgot to close tag";
+echo "</h1> forgot to close tag";
+echo "</h1> forgot to close tag";
+echo "</h1> forgot to close tag";
+echo "</h1> forgot to close tag";
 
