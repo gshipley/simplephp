e7ad5c6efacf747ec8ca000a7ea1d7e75a1f4965
grant shipley <grant shipley>
2016-11-18 11:57:45 -0500

    test

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
 echo "<h1>Welcome to the best application in the world.";
-
+echo "<br><h1>Hello, world</h1>";
+echo "<br><h1>Hello, world</h1>";echo "<br><h1>Hello, world</h1>";echo "<br><h1>Hello, world</h1>";
 
