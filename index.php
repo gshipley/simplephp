5101d771d45d955e134d27cf1bba826b95b069ba
grant shipley <grant shipley>
2016-11-18 11:53:53 -0500

    test

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
 echo "<h1>Welcome to the best application in the world.";
-echo "<br><h1>Welcome to the best application in the world.";
-echo "<br>Stop using h1 tags, it hurts my eyes";
+
 
