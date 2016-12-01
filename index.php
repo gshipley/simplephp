cc7747647b9800940a4d62e23035c2992daa8c40
Grant Shipley <gshipley@gmail.com>
2016-12-01 13:05:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
 echo "<h1>Welcome to the best application in the world.";
-echo "<br><h1>Hello, world</h1>";
-echo "<br><h1>Hello, world</h1>";echo "<br><h1>Hello, world</h1>";echo "<br><h1>Hello, world</h1>";
+
 
