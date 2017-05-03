fd7aa246db040f703abd4bfebc4290148815b4bb
Grant Shipley <gshipley@gmail.com>
2017-05-03 09:13:42 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
 
 echo "<h1>Best app ever!!";
-
-echo "<br><br>Day Block is awesome";
-
-echo "<br><BR><BR><h1>This is how I code</h1>";
-echo "<br><BR><BR><h1>This is how I code</h1>";
-echo "<br><BR><BR><h1>This is how I code</h1>";
